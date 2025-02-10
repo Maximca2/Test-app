398d4e39402ddd9673a8fa201d7974471f028c76
Maksym <max678797@gmail.com>
2025-02-10 21:52:31 +0200

    fix :quick fix

--- src/service/service.ts
+++ src/service/service.ts
@@ -2,7 +2,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_VPIC_API
 export async function fetchMakes() {
   try {
     const res = await fetch(
-      `${API_BASE_URL}/GetMakesForVehicleType/car?format=json`
+      `https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json`
     )
     const data = await res.json()
     return data.Results
@@ -14,7 +14,7 @@ export async function fetchMakes() {
 
 export async function fetchModel(makeId: string, year: string) {
   const res = await fetch(
-    `${API_BASE_URL}/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
+    `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
   )
   const data = await res.json()
 
